8bf0f9a0ffa7c83faffea271ce86b4cdf37ff3e2
Shimron <60049707+ShimronAlakkal@users.noreply.github.com>
2021-03-11 12:46:24 +0530

    Library (python Mysql project)

---
+++ Python Mysql Connection based Project/Library/login_signup.py
@@ -0,0 +1,92 @@
+
+import mysql.connector as mc
+import main_window
+
+connection = mc.connect(host='localhost',user='root',passwd='qwertyuiopasdfghjkl')
+crs = connection.cursor()
+
+crs.execute('create database IF NOT EXISTS botnet')
+crs.execute('use botnet ')
+crs.execute('create table IF NOT EXISTS user (name varchar(30) primary key , pass varchar(20) not null )')
+crs.execute('create table IF NOT EXISTS books (book varchar(50) not null ,author varchar(30), ID int(4) primary key, qty int(4) ,price int(4) )')
+connection.commit()
+try:
+	crs.execute("insert into user values('Admin', '12344' )")
+	crs.execute("insert into user values('Sample', '1234' )")
+	connection.commit()
+	crs.execute("insert into books values('In Search of Lost Time','Marcel Proust', 102 , 20,100 )")
+	crs.execute("insert into books values('Sapiens','Yuval Noah Harari', 103 , 231,650 )")
+	crs.execute("insert into books values('Harry Potter','JK Rowling', 101 , 13 ,100)")
+	crs.execute("insert into books values('The Great Gatsby','F. Scott Fitzgerald', 104 , 113 ,230)")
+	crs.execute("insert into books values('A Brief History of Time','Stephen Hawking', 105 , 303 ,230)")
+	crs.execute("insert into books values('Brief Answers to the Big Questions','Stephen Hawking', 106 , 13,100)")
+	connection.commit()
+except :
+	pass
+
+
+def userSingIn(Username, Password):
+
+	crs.execute("SELECT name,pass from USER where name like '"+str(Username)+"'" )
+	result = crs.fetchall()
+
+	if result[0][0] == Username and result[0][1] == Password:
+		main_window.mainView(0)
+	else:
+		print('Entered username or password is wrong. Please try again ')
+
+
+def userSignUp(Username, Password):
+	crs.execute("INSERT INTO USER values ('"+str(Username)+"','"+str(Password)+"')")
+	connection.commit()
+	main_window.mainView(0)
+
+
+def userCheck(Username):
+	crs.execute("SELECT name from USER where name like '"+str(Username)+"'" )
+	result = crs.fetchall()
+	try:
+		if result[0][0] == Username :
+			return True	
+	except :
+		return False
+	return False
+
+
+def Auth(val):
+
+	if val != 0:
+		print('\n Already a user?? press 1 \n New to Botnet?? press 2 \n or press 0 to exit \n ')
+		crs.execute('use Botnet')
+		auth = int(input('>>>'))
+		if auth == 1:
+			username = input('Please enter your Botnet username ')
+			passwd = str(input("Please enter your password for Botnet "))
+			
+			userSingIn(username,passwd)
+		elif auth == 2:
+			username = input('Please enter your username for Botnet ')
+			if userCheck(username) :	
+				print('Username Already Taken. Try Again ')
+				Auth(1)	
+			else:
+				passwd = eval(input("Please enter your password for Botnet "))		
+				userSignUp(username,passwd)
+			
+		elif auth == 0:
+			exit()
+
+	else:
+		print('''
+                     ___      __ ___
+                |) /\ | |\ | |__  |
+                |) \/ | | \| |__  |
+
+        ''')
+		print('\t\t Welcome to Botnet \n\t\t login/signup')
+		Auth(1)
+
+
+
+if __name__ == '__main__':
+	Auth(0)
---
+++ Python Mysql Connection based Project/Library/main_window.py
@@ -0,0 +1,119 @@
+# for the users account in BOTNET  library
+import mysql.connector as mc
+import 
+dbs = mc.connect(host='localhost',user='root',passwd='qwertyuiopasdfghjkl')
+crs = dbs.cursor()
+
+def productView():
+	print()
+	print("ID  |     Book Name          |  Qty") 
+	crs.execute("select id,book,qty from books ")
+	for i in crs:
+		print(i)
+
+
+def productPurchase():
+	print()# list altering 
+	print('BOOKS THAT WE HAVE AT BOTNET ...')
+	print()
+
+	productView()
+
+	print()
+
+	ask_b = int(input('which book do you want to buy ? (enter the  book id)'))
+	crs.execute ("select book,qty from books where id ="+str(ask_b))
+	for i in crs :
+		print('Book : ',i[0],' |  Qty : ',i[1])
+	print()
+
+	ask_n = int(input("how many do you want to buy "))
+	cnfrm = input('can we confirm your order ? (y/n)')
+
+	if cnfrm == 'y':
+		crs.execute ("select book,qty from books where id ="+str(ask_b))		
+		for i in crs:
+			if i[1] - ask_n <0: #negative qty is avoided
+				print('')
+				print('''Since your order contains a greater qty than that in the stocks,
+your order will be modified to match the stocks''')
+				ask_n -= -1*(i[1]-ask_n) 
+				print()
+				print('altered qty for buy = ',ask_n)
+				print()
+				crs.execute('update books set qty = qty-'+str(ask_n)+' where id='+str(ask_b))
+				print('thank you for your puchase from BOTNET ')
+				dbs.commit()
+				mainView(1)
+			else:
+				crs.execute('update books set qty = qty-'+str(ask_n)+' where id='+str(ask_b))
+				dbs.commit()
+				mainView(1)
+	else:
+		print()
+		mainView(1)
+
+
+def orderCancel():
+	c_ask = input('do you want to cancel your order ? (y/n)')
+	if c_ask == 'y':
+		print()
+		productView()
+		print()
+		sk = input('enter the id of the book to cancel order >>>  ')
+		y2= str(input(' how many books did you order ? ... '))
+		y = input('are you sure you want to cancel the order ? (y/n)')
+		print()
+		if y=='y':
+			crs.execute('update books set qty=qty+'+y2+' where id ='+str(sk)+';')
+			dbs.commit()
+			print('order cancelled  ...')
+			mainView(1)
+		else:
+			mainView(1)
+
+
+def mainView(val):
+	if val != 0:
+
+		print('''
+
+			1. View books that we have 
+			2. Purchase a book
+			3. cancel your order 
+			4. Logout
+
+			''')
+		opt = int(input('''
+	>>>
+			'''))
+		if opt == 1:
+			productView()
+			mainView(1)
+
+		elif opt == 2:
+			productPurchase()
+
+		elif opt == 3:
+			orderCancel()
+
+		elif opt == 4:
+			exit()
+	else:
+		print('''
+
+			WELCOME TO BOTNET
+
+     ########\  ##     ##         ##
+     ##     ##  ##     ##/####\   ##
+     ##     ##  ####   ##/   ##   ####
+     ########/  ####   ##    ##   ####
+     ########\  ##     ##    ##   ##
+     ##     ##  ##     ##    ##   ##
+     ##     ##  #####  ##    ##   #####
+     ########/  \####  ##    ##   \#### 
+
+    ''')
+
+		crs.execute('use botnet')
+		mainView(1)
